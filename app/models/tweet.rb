c9aa51f85b5454143101dd90b0e0d369218a3155
manato3 <iem73179@kwansei.ac.jp>
2025-12-01 18:59:14 +0900

    first commit

---
+++ app/models/tweet.rb
@@ -0,0 +1,11 @@
+class Tweet < ApplicationRecord
+has_many :likes, dependent: :destroy
+belongs_to :user
+has_many :comments, dependent: :destroy
+has_one_attached :image
+has_many :liked_users, through: :likes, source: :user
+#tweetsテーブルから中間テーブルに対する関連付け
+has_many :tweet_tag_relations, dependent: :destroy
+#tweetsテーブルから中間テーブルを介してTagsテーブルへの関連付け
+has_many :tags, through: :tweet_tag_relations, dependent: :destroy
+end
